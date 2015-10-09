822dbadf4ab6015a61faff1eee4432f603e782e8
CTS466856 <nishanth.vemula@cognizant.com>
2015-10-09 13:12:54 +0530

    Assignments Arrays Q2

---
+++ Day2_Morning/Array_Q3/Array_Q3/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array_Q3
+{
+	public class Person: IComparable
+	{
+		public string FirstName{ get; set; }
+
+		public string LastName{ get; set; }
+
+		public int Age{ get; set; }
+
+		public Person (string fname,string lname, int age)
+		{
+			this.LastName = lname;
+			this.FirstName = fname;
+			this.Age = age;
+		}
+
+		public int CompareTo (object obj)
+		{
+			if (obj is Person) {
+				return this.Age.CompareTo ((obj as Person).Age);  // compare person age
+			}
+			throw new ArgumentException ("Object is not a Person");
+		}
+	}
+}
+
---
+++ Day2_Morning/Array_Q3/Array_Q3/Program.cs
@@ -0,0 +1,50 @@
+﻿//	Create a custom type Person with FirstName, LastName and Age properties.
+//	Write a re-usable function that can Sort this Person[] Array by age. 
+//	Hint: Use IComparable and Array.Sort(arrObj)
+
+
+using System;
+using System.Collections;
+
+namespace Array_Q3
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Person[] P_Array = new Person[] {new Person ("Jack", "Osword", 21),
+				new Person ("Jack", "Daniels", 20),
+				new Person ("Nishanth", "Vemula", 22),
+				new Person ("Nikhil", "Ransoth", 34),
+				new Person ("Kisame", "Hishigaki", 28),
+				new Person ("Lewis", "Norman", 29),
+				new Person ("Kane", "Wills", 25),
+			};
+
+
+
+			Console.WriteLine ("-------- before sorting ----------");
+
+			for (int index = 0; index < P_Array.Length; index++) {
+
+				Console.WriteLine (P_Array [index].Age);
+			}
+
+			P_Array = Sort_Person_Array (P_Array);
+
+			Console.WriteLine ("-------- after sorting ----------");
+
+			for (int index = 0; index < P_Array.Length; index++) {
+				Console.WriteLine (P_Array [index].Age);
+			}
+		}
+
+		public static Person[] Sort_Person_Array (Person[] PersonArray)
+		{
+			
+			Array.Sort (PersonArray);
+			return PersonArray;
+
+		}
+	}
+}
