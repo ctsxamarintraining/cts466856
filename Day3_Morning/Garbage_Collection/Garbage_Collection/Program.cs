11d8eb67f42e7489463479f429d200e6f5431c48
CTS466856 <nishanth.vemula@cognizant.com>
2015-10-09 16:40:34 +0530

    Garbage Collection Assignment

---
+++ Day3_Morning/Garbage_Collection/Garbage_Collection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Garbage_Collection
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+			Console.WriteLine ("Memory at begining : {0}", GC.GetTotalMemory (false));
+
+			int[] values = new int[500000]{null};
+
+			Console.WriteLine ("Memory after introducing a value of type int[500000] : {0}", GC.GetTotalMemory (false));
+
+			GC.Collect ();
+
+			Console.WriteLine ("Memory after collecting garbage : {0}", GC.GetTotalMemory (false));
+
+		}
+	}
+}
