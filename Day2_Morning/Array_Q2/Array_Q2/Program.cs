2e2fdc5e4da510deefc50fbc48d73a4f33bf5115
CTS466856 <nishanth.vemula@cognizant.com>
2015-10-09 13:20:05 +0530

    Assignments Arrays Q2

---
+++ Day2_Morning/Array_Q2/Array_Q2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Array_Q2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[,] newArray = new int[5,2]{ {2,3},{5,7},{8,2},{9,1},{1,0}};
+			Print_Array (newArray);
+		}
+
+		static void Print_Array(Array theArray){
+			foreach (var elememt in theArray)
+				Console.WriteLine (elememt);
+		}
+	}
+}
