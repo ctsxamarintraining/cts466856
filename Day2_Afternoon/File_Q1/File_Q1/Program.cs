a1f495682cb7890197f8b1d09fb2dbc9dc2bb9ac
CTS466856 <Nishanth.Vemula@cognizant.com>
2015-10-11 08:34:02 +0530

    moved to correct folder
    generics was wrongly uploaded to day2 afternoon instead of day2 morning
    along with arrays

---
+++ Day2_Afternoon/File_Q1/File_Q1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace File_Q1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+			//File.Create ("NewFile.txt");
+			File.Delete("NewFile.txt");
+			File.Delete("NewFile2.txt");
+			using (StreamWriter source_FileWriter = new StreamWriter ("NewFile.txt")) {
+				source_FileWriter.WriteLine ("This is the first Line");
+				source_FileWriter.WriteLine ("This is the second Line");
+				source_FileWriter.WriteLine ("This is the third Line");
+				source_FileWriter.WriteLine ("This is the fourth Line");
+				source_FileWriter.WriteLine ("This is the fifth Line");
+			}
+
+			Console.WriteLine ("\n----- source file -----\n");
+
+			using (StreamReader source_StreamReader2 = new StreamReader ("NewFile.txt")) {
+				Console.WriteLine (source_StreamReader2.ReadToEnd());
+			}
+
+
+
+			List<string> lines = new List<string>();
+			using (StreamReader source_StreamReader = new StreamReader("NewFile.txt"))
+			{
+				string line;
+				while ((line = source_StreamReader.ReadLine()) != null)
+				{
+					lines.Add(line); 
+				}
+			}
+
+			lines.Reverse();
+
+			using (StreamWriter destination_StreamWriter = new StreamWriter ("NewFile2.txt")) {
+				foreach (string line in lines) {
+					destination_StreamWriter.WriteLine (line);
+				}
+			}
+
+
+			Console.WriteLine ("\n----- destination file -----\n");
+			using (StreamReader destination_StreamReader = new StreamReader ("NewFile2.txt")) {
+				Console.WriteLine (destination_StreamReader.ReadToEnd());
+			}
+			Console.ReadLine ();
+		}
+	}
+}
