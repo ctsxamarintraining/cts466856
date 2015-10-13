eff872680e60158a5d6a61e0b01a6dd0b2d8e2e0
CTS466856 <nishanth.vemula@cognizant.com>
2015-10-13 10:03:31 +0530

    Practice(2)_Q1

---
+++ day4_afternoon/Practice(2)/Practice(2)-Q1/Program.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// Write a program that reads an integer number and calculates and prints its square root.
+/// If the number is invalid or negative, print "Invalid number”.
+/// In all cases finally print "Goodbye". Use try-catch-finally.
+/// </summary>
+
+using System;
+
+namespace Practice2Q1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int inputInt = 0;
+
+			try {
+				Console.WriteLine ("Enter an Integer to find it's SquareRoot: ");
+				inputInt = Convert.ToInt32 (Console.ReadLine ());
+
+				Console.WriteLine("\nThe SquareRoot of {0} is {1}",inputInt,Math.Sqrt(inputInt));
+
+			} catch (FormatException) {	
+				//Console.WriteLine ("Exception : {0}\ninput value must be a positive integer", fEx.Message);
+				Console.WriteLine ("InvalidNumber");
+			} catch(ArgumentException){
+				//Console.WriteLine ("Exception : {0}\ninput value must be an integer", ArgEx.Message);
+				Console.WriteLine ("InvalidNumber");
+			}
+			catch (Exception Ex) {
+				Console.WriteLine (Ex.Message);
+			}
+			finally{
+				Console.WriteLine ("Goodbye");
+			}
+
+			Console.ReadLine ();
+		}
+	}
+}
