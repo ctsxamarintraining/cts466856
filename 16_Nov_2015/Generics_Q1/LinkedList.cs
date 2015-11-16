265b485e4c0ccb503fea6c9f90011b1789fd55b5
CTS466856 <nishanth.vemula@cognizant.com>
2015-11-16 15:05:34 +0530

    practice 16 Nov

---
+++ 16_Nov_2015/Generics_Q1/LinkedList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Generics_Q1
+{
+
+	public class MyEnumerable<T> : IEnumerable
+	{
+		public IEnumerator GetEnumerator ()
+		{
+			return new LinkedList<T> ();
+		}
+	}
+
+	public class LinkedList<T> :IEnumerator
+	{
+		public bool MoveNext ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Reset ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public object Current {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		private class Node
+		{
+			// Each node has a reference to the next node in the list.
+			public Node next;
+			// Each node holds data of type T.
+			public T data;
+		}
+
+		// The list is initially empty.
+		private Node head;
+
+		public void printAllNodes ()
+		{
+			Node current = head;
+			while (current != null) {
+				Console.WriteLine ("\t{0}",current.data);
+				current = current.next;
+			}
+		}
+
+		// Add a node at the beginning of the list with t as its data value.
+		public void AddFirst (T data)
+		{ 
+			Node toAdd = new Node ();
+
+			toAdd.data = data;
+			toAdd.next = head;
+
+			head = toAdd;
+		}
+
+		// Add a node at the beginning of the list with t as its data value.
+		public void AddLast (T data)
+		{
+			// this is the first node
+			if (head == null) {
+				head = new Node ();
+
+				head.data = data;
+				head.next = null;
+			} else {
+				Node toAdd = new Node ();
+				toAdd.data = data;
+
+				Node current = head;
+				while (current.next != null) {
+					current = current.next;
+				}
+
+				current.next = toAdd;
+			}
+		}
+
+		// remove node at last of the list
+		public void RemoveLast (){
+			if (head != null) {
+				Node current = head;
+				Node nextNode = current.next;
+				while (nextNode != null) {
+					
+					if (nextNode.next == null) {
+						current.next = null;
+						return;
+					}
+					current = nextNode;
+					nextNode = nextNode.next;
+
+				}
+			}
+			else
+			throw new System.ArgumentException("List is empty cannot remove any object");
+		}
+
+		// remove node at begining of the list
+		public void RemoveFirst (){
+			if (head != null) {
+				head = head.next;
+			}
+			else
+			throw new System.ArgumentException("List is empty cannot remove any object");
+
+		}
+
+
+	}
+}
+
---
+++ 16_Nov_2015/Generics_Q1/Program.cs
@@ -0,0 +1,70 @@
+﻿//	Create a simple Linked list (MyLinkedList<T>) with following methods
+//	void Add(T)
+//	void Remove(T)
+//	T Find(T)
+//	int Count{get;set;}
+
+using System;
+
+namespace Generics_Q1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/*
+			Console.WriteLine("my list 1 - Add First:");
+			Console.WriteLine ();
+			LinkedList<string> myList1 = new LinkedList<string>();
+
+			myList1.AddFirst("Hello");
+			myList1.AddFirst("Magical");
+			myList1.AddFirst("World");
+			myList1.AddFirst ("This");
+			myList1.AddFirst ("is");
+			myList1.AddFirst ("Good");
+			myList1.printAllNodes();
+
+			Console.WriteLine();
+
+			Console.WriteLine("my list 2 - Add Last:");
+			Console.WriteLine ();
+			LinkedList<string> myList2 = new LinkedList<string>();
+
+			myList2.AddLast("Hello");
+			myList2.AddLast("Magical");
+			myList2.AddLast("World");
+			myList2.AddLast ("This");
+			myList2.AddLast ("is");
+			myList2.AddLast ("Good");
+			myList2.printAllNodes();
+
+
+			Console.WriteLine("my list 1 - remove First:");
+			Console.WriteLine ();
+
+			myList1.RemoveFirst ();
+			myList1.printAllNodes();
+
+			Console.WriteLine("my list 2 - remove Last:");
+			Console.WriteLine ();
+
+			myList2.RemoveLast ();
+			myList2.printAllNodes();
+
+
+			Console.ReadLine();
+			*/
+
+			MyEnumerable<string> newEnumerable = MyEnumerable<string>;
+			LinkedList<string> theList = (newEnumerable.GetEnumerator as LinkedList<string>);
+
+			theList.AddFirst ("string1");
+			theList.AddFirst ("string2");
+			theList.AddFirst ("string3");
+			theList.AddFirst ("string4");
+			theList.AddFirst ("string5");
+
+		}
+	}
+}
