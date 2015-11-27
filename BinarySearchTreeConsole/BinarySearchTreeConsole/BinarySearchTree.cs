ea94e6a1a097755976dbe3ef29774eedfc977fcc
CTS466856 <nishanth.vemula@cognizant.com>
2015-11-27 12:03:15 +0530

    upd
    a

---
+++ BinarySearchTreeConsole/BinarySearchTreeConsole/BinarySearchTree.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections;
+
+namespace BinarySearchTreeConsole
+{
+	public class Node
+	{
+		public Node greaterSubNode;
+		public Node lesserSubNode;
+		public int NodeValue;
+
+		public Node ()
+		{
+		}
+
+		public Node (int data)
+		{
+			NodeValue = data;
+			greaterSubNode = null;
+			lesserSubNode = null;
+		}
+
+		public Node (Node theinputnode)
+		{
+			greaterSubNode = theinputnode.greaterSubNode;
+			lesserSubNode = theinputnode.lesserSubNode;
+			NodeValue = theinputnode.NodeValue;
+		}
+	}
+
+	public class BSTEnumerator : IEnumerator
+	{
+		Node rootNode;
+		Node currentNode;
+		bool first_flag = true;
+
+		public BSTEnumerator (Node head)
+		{
+			rootNode = head;
+			currentNode = head;
+		}
+
+		#region IEnumerator implementation
+
+		public bool MoveNext ()
+		{
+			if (first_flag) {
+				first_flag = false;
+				return true;
+			} else
+				return false;
+		}
+
+		public void Reset ()
+		{
+			currentNode = rootNode;
+		}
+
+		public object Current {
+			get {
+				return currentNode;
+			}
+		}
+
+		#endregion
+	}
+
+	public class BinarySearchTree : IEnumerable
+	{
+		public Node root;
+		public Node current_Node;
+
+
+		#region IEnumerable implementation
+
+		public IEnumerator GetEnumerator ()
+		{
+			return new BSTEnumerator (root);
+		}
+
+		#endregion
+
+
+		public bool Add (int data)
+		{
+			Node toAdd = new Node ();
+			toAdd.NodeValue = data;
+			toAdd.lesserSubNode = null;
+			toAdd.greaterSubNode = null;
+
+			if (root == null) {
+				root = toAdd;
+				current_Node = toAdd;
+				return true;
+			}
+
+			current_Node = root;
+			while (true) {
+				if (current_Node.NodeValue == data) {
+					return false;
+
+				} else if (current_Node.NodeValue > data) {
+					if (current_Node.lesserSubNode == null) {
+						current_Node.lesserSubNode = toAdd;
+						current_Node = toAdd;
+					
+						return true;
+
+					} else {
+						current_Node = current_Node.lesserSubNode;
+					}
+				} else {
+					if (current_Node.greaterSubNode == null) {
+						current_Node.greaterSubNode = toAdd;
+						current_Node = toAdd;
+
+						return true;
+
+					} else {
+						current_Node = current_Node.greaterSubNode;
+					}
+				}
+			}
+		}
+
+		public bool Search (int searchValue)
+		{
+			bool searchflag = false;
+			current_Node = root;
+
+			while (true) {
+				if (current_Node.NodeValue == searchValue) {
+					searchflag = true;
+					break;
+				} else if (current_Node.NodeValue > searchValue) {
+					if (current_Node.lesserSubNode == null) {
+						searchflag = false;
+						break;
+					} else {
+						current_Node = current_Node.lesserSubNode;
+					}
+				} else if (current_Node.NodeValue < searchValue) {
+					if (current_Node.greaterSubNode == null) {
+						searchflag = false;
+						break;
+					} else {
+						current_Node = current_Node.greaterSubNode;
+					}
+				}
+			}
+			return searchflag;
+		}
+
+		public void Delete (int deleteValue)
+		{
+			root = DeleteKey (root, deleteValue);
+		}
+
+		private Node DeleteKey (Node theRootNode, int Key)
+		{
+			
+			if (theRootNode == null)
+				return theRootNode;
+
+
+			if (Key < theRootNode.NodeValue) {
+				
+				theRootNode.lesserSubNode = DeleteKey (theRootNode.lesserSubNode, Key);
+
+			} else if (Key > theRootNode.NodeValue) {
+				
+				theRootNode.greaterSubNode = DeleteKey (theRootNode.greaterSubNode, Key);
+
+			} else {
+				
+				if (theRootNode.lesserSubNode == null) {
+					return theRootNode.greaterSubNode;
+				} else if (theRootNode.greaterSubNode == null) {
+					return theRootNode.lesserSubNode;
+				}
+
+				// node with two children: smallest in the right subtree
+				Node temp = new Node (theRootNode.greaterSubNode);
+				while (true) {
+					if (temp.lesserSubNode == null)
+						break;
+					temp = temp.lesserSubNode;
+				}
+
+				// Copy the inorder successor's content to this node
+
+				theRootNode.NodeValue = temp.NodeValue;
+
+				// Delete the inorder successor
+				theRootNode.greaterSubNode = DeleteKey (theRootNode.greaterSubNode, temp.NodeValue);
+			}
+			return theRootNode;
+		}
+
+	}
+}
+//			while (true) {
+//
+//
+//				if (current_Node.NodeValue == deleteValue) {
+//
+//					// delete current node or deletable node using in-order succession
+//
+//					Node replacingNode = new Node (current_Node);
+//
+//					if(replacingNode.greaterSubNode == null && replacingNode.lesserSubNode == null){
+//						current_Node = null;
+//						deleteflag = true;
+//						break;
+//					}else if (replacingNode.greaterSubNode == null) {
+//						current_Node = replacingNode.lesserSubNode;
+//					} else if (replacingNode.lesserSubNode == null) {
+//						current_Node = replacingNode.greaterSubNode;
+//					} else {
+//
+//						replacingNode = replacingNode.greaterSubNode;
+//
+//						while (true) {
+//							if (replacingNode.lesserSubNode.lesserSubNode == null) {
+//								current_Node.NodeValue = replacingNode.lesserSubNode.NodeValue;
+//								replacingNode.lesserSubNode = null;
+//								break;
+//							}
+//						}
+//
+//						deleteflag = true;
+//						count--;
+//						break;
+//					}
+//
+//
+//
+//
+//				} else if (current_Node.NodeValue > deleteValue) {
+//					if (current_Node.lesserSubNode == null) {
+//						break;
+//					} else {
+//						current_Node = current_Node.lesserSubNode;
+//					}
+//				} else if (current_Node.NodeValue < deleteValue) {
+//					if (current_Node.greaterSubNode == null) {
+//						break;
+//
+//					} else {
+//						current_Node = current_Node.greaterSubNode;
+//					}
+//				}
+//			}
---
+++ BinarySearchTreeConsole/BinarySearchTreeConsole/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace BinarySearchTreeConsole
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			BinarySearchTree TheBST = new BinarySearchTree ();
+			if (TheBST.Add (10)) {
+				Console.WriteLine ("Added 10");
+			} else {
+				Console.WriteLine ("Could Not Add 10 as it is already present");
+			}
+			if (TheBST.Add (15)) {
+				Console.WriteLine ("Added 15");
+			} else {
+				Console.WriteLine ("Could Not Add 15");
+			}
+			if (TheBST.Add (18)) {
+				Console.WriteLine ("Added 18");
+			} else {
+				Console.WriteLine ("Could Not Add 18");
+			}
+			if (TheBST.Add (12)) {
+				Console.WriteLine ("Added 12");
+			} else {
+				Console.WriteLine ("Could Not Add 12");
+			}
+			if (TheBST.Add (17)) {
+				Console.WriteLine ("Added 17");
+			} else {
+				Console.WriteLine ("Could Not Add 17");
+			}
+			if (TheBST.Add (7)) {
+				Console.WriteLine ("Added 7");
+			} else {
+				Console.WriteLine ("Could Not Add 70");
+			}
+
+
+			if (TheBST.Search (17)) {
+				Console.WriteLine ("found node containing 17. Verifiying its value {0}", TheBST.current_Node.NodeValue);
+			} else {
+				Console.WriteLine ("not found 17");
+			}
+
+			TheBST.Delete (17);
+
+			if (TheBST.Search (17)) {
+				Console.WriteLine ("found node containing 17. Verifiying its value {0}", TheBST.current_Node.NodeValue);
+			} else {
+				Console.WriteLine ("not found 17");
+			}
+			if (TheBST.Search (10)) {
+				Console.WriteLine ("found node containing 10. Verifiying its value {0}", TheBST.current_Node.NodeValue);
+			} else {
+				Console.WriteLine ("not found 10");
+			}
+
+			Console.ReadLine ();
+		}
+	}
+}
